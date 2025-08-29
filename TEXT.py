d7b6cda7a09678ac7f811db4e054e91f361d3b13
quangkhung <quangmk999@gmail.com>
2025-08-29 11:44:30 +0700

    Update requirements Project

--- TEXT.py
+++ TEXT.py
@@ -3,18 +3,25 @@ from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
 import cv2
 
 # Tiêu đề
-st.title("📷 Camera Streaming qua Web (WebRTC + OpenCV)")
-
-
-# Tạo class xử lý video
-class VideoTransformer(VideoTransformerBase):
-    def transform(self, frame):
-        img = frame.to_ndarray(format="bgr24")
+st.title("ĐỒ ÁN TỐT NGHIỆP")
+cap=cv2.VideoCapture(0)
+frame_placehodler = st.empty()
+#Tạo class xử lý video
+#class VideoTransformer(VideoTransformerBase):
+#   def transform(self, frame):
+#        img = frame.to_ndarray(format="bgr24")
 
         # 👉 Ví dụ: chuyển sang grayscale
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        return cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)  # Trả về ảnh màu để hiển thị
-
+#        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#        return cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)  # Trả về ảnh màu để hiển thị
 
 # Kích hoạt camera từ trình duyệt
-webrtc_streamer(key="camera", video_transformer_factory=VideoTransformer)
+#webrtc_streamer(key="camera", video_transformer_factory=VideoTransformer)
+while True:
+    ret, frame = cap.read()
+    if ret:
+        st.write("khong mo camera")
+        break
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame_placeholder = frame_placeholder.image(frame, channels="RGB")
+        cap.release()
